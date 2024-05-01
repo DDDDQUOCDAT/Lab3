2477f80817cce669ed93af99f4ea685a063ed814
Samuel <DDDD06.23@ichat.sp.edu.sg>
2024-05-01 21:36:41 +0800

    test for bubble_sort

--- Lab3.py
+++ Lab3.py
@@ -11,8 +11,11 @@ def bubble_sort(arr, sorting_order):
 
     # Get number of elements in the list
     n = len(arr_result)
+    for x in arr:
+        if not isinstance (x,int):
+            return 2
 
-    if n < 10:
+    if (n < 10 and n > 0):
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,8 +36,10 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
-    else:
-        arr_result = -1
+    elif (n>=10):
+        return 1
+    else : 
+        return 0
 
     return arr_result
 
